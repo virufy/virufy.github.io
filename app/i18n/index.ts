fa0badd1b7582b663518d0cabc3ef1dc401292ba
DC Breckle <cookmaster22@yahoo.com>
2025-04-20 18:21:49 -0500

    'Reimplementing spanish'

--- app/i18n/index.ts
+++ app/i18n/index.ts
@@ -3,11 +3,13 @@ import enMetadata from './en/index';
 import jaMetadata from './ja/index';
 import arMetadata from './ar/index';
 import { type I18nData } from './types';
+import esMetadata from './es';
 
 const i18n: Record<Locale, I18nData> = {
   en: enMetadata,
   ja: jaMetadata,
   ar: arMetadata,
+  es: esMetadata,
 };
 
 export const usei18n = (locale: Locale) => {
