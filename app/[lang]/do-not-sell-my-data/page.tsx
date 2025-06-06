5e2eef039762a52708724f09d0e504032f664abb
Howardlin0726 <howard.lin@virufy.org>
2025-06-06 15:29:31 -0700

    Update Footer Links and Add “Do Not Sell My Data” Page (#116)
    * New refreshed background image, updated Amil's headshot, and removed top navbar
    
    * Add do-not-sell-my-data page and update footer links
    
    ---------
    
    Co-authored-by: Howardlin26 <linhungc@usc.edu>

---
+++ app/[lang]/do-not-sell-my-data/page.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { type Locale } from '@/i18n-config';
+import { usei18n } from '../../i18n';
+import Title from '../components/Title';
+import AccordionItem from '../components/AccordionItem';
+
+const DoNotSellMyDataPage = ({ params: { lang } }: { params: { lang: Locale } }) => {
+  const {
+    footer: { personalInfo },
+  } = usei18n(lang);
+
+  return (
+    <div className="relative mt-[-6rem] py-20 flex flex-col items-center px-5 bg-gradient-to-b from-[#25497d] via-[#3468b2] to-[#3468b2]">
+      {/* Title */}
+      <div className="py-20">
+        <Title
+          H="h5Modals"
+          Text={personalInfo.title}
+          TitleClassProps="mt-[30px] mx-auto mb-[30px] text-white"
+        />
+      </div>
+
+      {/* Accordion Content */}
+      <div className="w-full max-w-md rounded-lg border-b bg-black bg-opacity-[28%] 
+      text-xs last:border-b-0 md:max-w-2xl md:text-base lg:max-w-4xl xl:max-w-5xl">
+        {personalInfo.body.map((detail, index) => (
+          <AccordionItem key={index} {...detail} lang={lang} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default DoNotSellMyDataPage;
