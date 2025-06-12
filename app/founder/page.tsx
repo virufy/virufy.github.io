f50b2f00793ce5ce5594f6bb7a73c0a6544e384a
DC Breckle <cookmaster22@yahoo.com>
2025-06-12 15:05:35 -0500

    made founder go to amils-story

--- app/founder/page.tsx
+++ app/founder/page.tsx
@@ -3,6 +3,8 @@ import { i18n } from '@/i18n-config';
 import RedirectWithSpinner from '../[lang]/components/RedirectWithSpinner';
 
 const FounderPage = () => {
-  return <RedirectWithSpinner targetUrl={`/${i18n.defaultLocale}/founder`} />;
+  return (
+    <RedirectWithSpinner targetUrl={`/${i18n.defaultLocale}/amils-story`} />
+  );
 };
 export default FounderPage;
