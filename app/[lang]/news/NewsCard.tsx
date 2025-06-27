8a6c5ed1f3a00b694607107d89934b384f0e1e59
Snigdha6698 <89868871+Snigdha6698@users.noreply.github.com>
2025-06-27 17:42:07 -0500

    W50-431 required changes are made

--- app/[lang]/news/NewsCard.tsx
+++ app/[lang]/news/NewsCard.tsx
@@ -4,14 +4,14 @@ import Link from 'next/link';
 const NewsCard = ({ title, date, subText, url, linkText }: NewsCard) => {
   return (
     <>
-      <h3 className="max-w-lg text-xl font-medium lg:h-28 xl:h-20">{title}</h3>
-      <div className="py-4">{subText}</div>
-      <div className="items-center space-y-6 md:flex md:flex-row md:justify-between md:space-y-0">
+      <h3 className="max-w-lg text-xl text-black font-bold lg:h-28 xl:h-20">{title}</h3>
+      <div className="py-4" >{subText}</div>
+      <div className="items-center space-y-6 md:flex md:flex-row md:justify-between font-bold text-black md:space-y-0">
         <ul className="list-inside list-disc pb-6">
           <li>{date}</li>
         </ul>
         <Link
-          className="rounded-3xl bg-white px-4 py-2 text-[#123d62] sm:px-8"
+          className="rounded-3xl bg-[#3578DE] px-4 py-2 text-white sm:px-8"
           target="_blank"
           href={url}
         >
--- app/[lang]/news/page.tsx
+++ app/[lang]/news/page.tsx
@@ -23,6 +23,7 @@ const NewsPage = ({ params: { lang } }: { params: { lang: Locale } }) => {
       content="Stay updated with the latest news and press releases about Virufy's advancements in AI healthcare innovation."
     />
   </Head>
+ 
       <div className="relative -top-24">
         {/* Hero Section */}
         <section>
@@ -36,8 +37,9 @@ const NewsPage = ({ params: { lang } }: { params: { lang: Locale } }) => {
             />
 
             <div className="mx-auto flex max-w-xl flex-col items-center justify-center space-y-8 px-10 py-64 text-center font-medium opacity-95">
-              <h1 className="text-4xl text-green-500 sm:text-5xl font-bold">
+              <h1 className="text-4xl sm:text-5xl text-white font-bold">
                 {pressReleaseSection.title}
+  
               </h1>
             </div>
           </div>
@@ -51,7 +53,7 @@ const NewsPage = ({ params: { lang } }: { params: { lang: Locale } }) => {
               {pressReleaseCards.map((pressRelease, i) => (
                 <div
                   key={i}
-                  className="rounded-xl bg-[#00000060] px-10 py-8 text-left text-white"
+                  className="rounded-xl bg-white px-10 py-8 text-left text-white"
                 >
                   <NewsCard {...pressRelease} />
                 </div>
