bf552c94c5112072493cc27413ad60eae2978f35
DC Breckle <cookmaster22@yahoo.com>
2025-07-10 21:32:02 -0500

    added modal, need to change messages

---
+++ app/i18n/ar/cookiemodal.ts
@@ -0,0 +1,8 @@
+import { type CookieModal } from '../types/cookiemodal';
+
+const cookieModal: CookieModal = {
+  text: 'this is text',
+  yes: 'yes',
+  no: 'no',
+};
+export default cookieModal;
---
+++ app/i18n/ja/cookiemodal.ts
@@ -0,0 +1,8 @@
+import { type CookieModal } from '../types/cookiemodal';
+
+const cookieModal: CookieModal = {
+  text: 'this is jp text',
+  yes: 'no',
+  no: 'yes',
+};
+export default cookieModal;
