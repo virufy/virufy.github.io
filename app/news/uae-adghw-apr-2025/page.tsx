8cd8f4579cde7aee77dec2cf7199fc192913507a
DC Breckle <cookmaster22@yahoo.com>
2025-07-12 03:41:44 -0500

    fixed other link

--- app/news/uae-adghw-apr-2025/page.tsx
+++ app/news/uae-adghw-apr-2025/page.tsx
@@ -1,11 +1,11 @@
 'use client';
 import { i18n } from '@/i18n-config';
-import RedirectWithSpinner from '../../[lang]/components/RedirectWithSpinner';
+import RedirectWithSpinner from '@/app/[lang]/components/RedirectWithSpinner';
 
 const NewsRedirect = () => {
   return (
     <RedirectWithSpinner
-      targetUrl={`/${i18n.defaultLocale}/news/Abu-Dhabi-June-2025`}
+      targetUrl={`/${i18n.defaultLocale}/news/uae-adghw-apr-2025`}
     />
   );
 };
