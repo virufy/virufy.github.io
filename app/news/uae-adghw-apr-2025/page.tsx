0dd910891bcda3b3eb2667d96061b86990296116
Snigdha6698 <89868871+Snigdha6698@users.noreply.github.com>
2025-07-14 13:54:36 -0500

    resolved merge conflicts as mentioned

--- app/news/uae-adghw-apr-2025/page.tsx
+++ app/news/uae-adghw-apr-2025/page.tsx
@@ -5,11 +5,9 @@ import RedirectWithSpinner from '../../[lang]/components/RedirectWithSpinner';
 const NewsRedirect = () => {
   return (
     <RedirectWithSpinner
-<<<<<<< HEAD
-      targetUrl={`/${i18n.defaultLocale}/news/Abu-Dhabi-June-2025`}
-=======
+
       targetUrl={`/${i18n.defaultLocale}/news/uae-adghw-apr-2025`}
->>>>>>> origin/Testing
+
     />
   );
 };
