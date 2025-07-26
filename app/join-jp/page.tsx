dc8f08a90792e2df969a1828283b004c5d51567b
DC Breckle <cookmaster22@yahoo.com>
2025-07-26 17:17:44 -0500

    reimplemented join-jp and [lang]/join-jp

---
+++ app/join-jp/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+import RedirectWithSpinner from '@/app/[lang]/components/RedirectWithSpinner';
+
+const ApplyPageJP = () => {
+  return (
+    <RedirectWithSpinner targetUrl={`https://forms.gle/nxGbL1pd1R8Q27qe7`} />
+  );
+};
+export default ApplyPageJP;
