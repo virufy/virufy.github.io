2d9e8fc03aa2a8a954d75f8fdeca98deb7aa8f55
DC Breckle <cookmaster22@yahoo.com>
2025-08-11 20:55:01 -0500

    merged

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,6 @@
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 const supportedLanguages = ['ja', 'en', 'es', 'ar'];
-('use client');
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
-const supportedLanguages = ['ja', 'en', 'es', 'ar'];
 
 const RootPage = () => {
   const router = useRouter();
