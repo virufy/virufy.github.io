a0f0217f98c09a2ae882f44ba9974599dbde8f00
AngdroRyu <cookmaster22@yahoo.com>
2026-03-05 14:47:07 -0600

    fixed missing translations for supporters

--- app/i18n/es/sevensupporters.ts
+++ app/i18n/es/sevensupporters.ts
@@ -3,16 +3,133 @@ import type { sevensupporters } from '../types/sevensupporters';
 const sevensupporters: sevensupporters = {
   introSection: {
     tag: 'Alianzas Globales',
-    title: ['Nuestros Colaboradores'],
-    text: 'Virufy colabora con instituciones académicas líderes, innovadores de la industria y organizaciones sin fines de lucro en todo el mundo para avanzar en la detección de la salud respiratoria impulsada por IA y hacer que la detección temprana sea accesible para todos.',
+    title: ['Nuestros', 'Patrocinadores'],
+    text: 'Virufy colabora con instituciones académicas líderes, innovadores de la industria y organizaciones sin fines de lucro en todo el mundo para avanzar en el cribado de salud respiratoria impulsado por inteligencia artificial y hacer que la detección temprana sea accesible para todos.',
   },
   banner: {
     title: 'Únete a Nuestra Misión',
     buttontext: 'Conviértete en Socio',
     text: [
-      'Asóciate con Virufy para avanzar en la tecnología de detección de la salud respiratoria y generar un impacto global. Juntos, podemos ayudar a detectar enfermedades antes y salvar vidas en todo el mundo.',
+      'Asóciate con Virufy para avanzar en la tecnología de detección de salud respiratoria y generar un impacto global. Juntos podemos ayudar a detectar enfermedades antes y salvar vidas en todo el mundo.',
     ],
+    link: '/join-us',
   },
+  SupporterImg: [
+    {
+      img: '../../logos_transparent/h4r.png',
+      alt: 'H4R',
+      link: 'https://h4r.stanford.edu/',
+    },
+    {
+      img: '../../logos_transparent/aws.png',
+      alt: 'AWS',
+      link: 'https://aws.amazon.com/',
+    },
+    {
+      img: '../../logos_transparent/Microsoft.png',
+      alt: 'Microsoft',
+      link: 'https://www.microsoft.com/',
+    },
+    {
+      img: '../../logos/response-innovation-lab.webp',
+      alt: 'Response Innovation Lab',
+      link: 'https://www.responseinnovationlab.com/',
+    },
+    {
+      img: '../../logos_transparent/one-young-world.png',
+      alt: 'One Young World',
+      link: 'https://www.oneyoungworld.com/counsellors',
+    },
+    {
+      img: '../../logos_transparent/drive.png',
+      alt: 'Drive',
+      link: 'https://drive.hhs.gov/',
+    },
+    {
+      img: '../../logos_transparent/university-fukui.png',
+      alt: 'University Fukui',
+      link: 'https://www.u-fukui.ac.jp/eng',
+    },
+    {
+      img: '../../logos_transparent/idime.png',
+      alt: 'Idime',
+      link: 'https://idime.com.co/',
+    },
+    {
+      img: '../../logos_transparent/pillsbury.png',
+      alt: 'Pillsbury',
+      link: 'https://www.pillsburylaw.com/en/',
+    },
+    {
+      img: '../../logos_transparent/xoor.png',
+      alt: 'Xoor',
+      link: 'https://xoor.io/en/',
+    },
+    {
+      img: '../../logos_transparent/sherlock-communications.png',
+      alt: 'Sherlock Communications',
+      link: 'https://www.sherlockcomms.com/',
+    },
+    {
+      img: '../../logos_transparent/nuvu.png',
+      alt: 'Nuvu',
+      link: 'https://www.nuvu.cc/',
+    },
+    {
+      img: '../../logos_transparent/spi.png',
+      alt: 'SPI',
+      link: 'https://spiamericas.com/en/home/',
+    },
+    {
+      img: '../../logos_transparent/devlights.png',
+      alt: 'Devlights',
+      link: 'https://www.devlights.com/',
+    },
+    {
+      img: '../../logos_transparent/fenwick.png',
+      alt: 'Fenwick',
+      link: 'https://www.fenwick.com/',
+    },
+    {
+      img: '../../logos_transparent/olarte-moure.png',
+      alt: 'Olarte Moure',
+      link: 'https://www.olartemoure.com/',
+    },
+    {
+      img: '../../logos_transparent/locked-jar.png',
+      alt: 'Locked Jar',
+      link: 'https://www.lockedjar.com/',
+    },
+    {
+      img: '../../logos/takemoto-lab.webp',
+      alt: 'Takemoto Lab',
+      link: 'http://www.hisac.u-fukui.ac.jp/imlabo/home.html',
+    },
+    {
+      img: '../../logos/life-science.webp',
+      alt: 'Life Science Washington',
+      link: 'https://www.lswinstitute.org/',
+    },
+    {
+      img: '../../logos/candido-costa-and-cancio.webp',
+      alt: 'Candido Costa & Cancio',
+      link: 'https://cccadv.com.br/site/en/home/',
+    },
+    {
+      img: '../../logos/arkangel-ai.webp',
+      alt: 'Arkangel Ai',
+      link: 'https://www.arkangel.ai/',
+    },
+    {
+      img: '../../logos/nexus-tax-services.webp',
+      alt: 'Nexus Tax Services',
+      link: 'https://nexustaxaccountants.com/',
+    },
+    {
+      img: '../../logos_transparent/cj-partners.png',
+      alt: 'CJ Partners',
+      link: 'https://www.cj-partners.com/',
+    },
+  ],
 };
-
 export default sevensupporters;
