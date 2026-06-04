bb1a726af5ae595fae5ebed1931a4e45224b9acb
Evan Luong <evantluong105@gmail.com>
2026-06-04 12:35:37 -0700

    Update footer styling for V7 (#239)
    * Update footer styling for V7
    
    * Add Candid transparency seal to footer

--- app/[lang]/components/footer/NonProfitDisclaimers.jsx
+++ app/[lang]/components/footer/NonProfitDisclaimers.jsx
@@ -1,70 +1,78 @@
 import Text from '../Text';
 import { basePath } from '@/next.config.mjs';
-import { CandidSeal } from '@/public/images/footer/index';
 import ExportedImage from 'next-image-export-optimizer';
-import Link from 'next/link';
-export default function NonProfitDisclaimers({nonProfitSection}) {
-    return (
-        <>
-        <div className="flex justify-center mt-4">
-          <Link
-            target="_blank"
-            href="https://www.guidestar.org/profile/shared/f0cf98a8-3b5a-495f-86c0-d2d58564086c"
-            className="block"
-          >
+import { CandidSeal } from '@/public/images/footer/index';
+
+export default function NonProfitDisclaimers({ nonProfitSection }) {
+  return (
+    <>
+      <div className="align-center mt-10 flex w-full flex-col gap-7 text-[#111827] lg:hidden">
+        <div className="flex justify-center">
           <ExportedImage
             src={CandidSeal}
-            alt="GuideStar Profile"
-            className="mx-auto"
+            alt="Candid Gold Transparency Seal"
+            className="h-[100px] w-auto object-contain"
             basePath={basePath}
-            width={100}  
-            height={100}
           />
-          </Link>
         </div>
-        <div className="align-center mt-10 flex w-full flex-col gap-7 text-white lg:hidden">
-            <div className="align-center m-auto max-w-4xl text-center text-[25px]">
-              <Text Text={nonProfitSection.title} Style="" TextClassProps="" />
-            </div>
-            <div className="align-center m-auto mx-12 mt-4 max-w-4xl text-center">
-              <Text
-                TextClassProps=""
-                Text={nonProfitSection.textOne}
-                Style="normal"
-              />
-            </div>
-            <div className="align-center m-auto mx-12 mt-4 max-w-4xl text-center">
-              <Text
-                TextClassProps=""
-                Text={nonProfitSection.textTwo}
-                Style="normal"
-              />
-            </div>
-          </div>
-          <div className="align-center mt-10 hidden w-full flex-col gap-7 text-white lg:flex">
-            <div className="align-center m-auto max-w-4xl text-center font-semibold">
-              <Text
-                Text={nonProfitSection.title}
-                Style="small"
-                TextClassProps=""
-              />
-            </div>
-            <div className="align-center m-auto max-w-4xl text-center">
-              <Text
-                TextClassProps=""
-                Text={nonProfitSection.textOne}
-                Style="normalCardHome"
-              />
-            </div>
-            <div className="align-center m-auto max-w-4xl text-center">
-              <Text
-                TextClassProps=""
-                Text={nonProfitSection.textTwo}
-                Style="normalCardHome"
-              />
-            </div>
-          </div>
-        </>
 
-    )
+        <div className="align-center m-auto max-w-4xl text-center text-[25px]">
+          <Text Text={nonProfitSection.title} Style="" TextClassProps="" />
+        </div>
+
+        <div className="align-center m-auto mx-12 mt-4 max-w-4xl text-center">
+          <Text
+            TextClassProps=""
+            Text={nonProfitSection.textOne}
+            Style="normal"
+          />
+        </div>
+
+        <div className="mt-4 w-full border-t border-gray-300"></div>
+
+        <div className="align-center m-auto mx-12 mt-4 max-w-4xl text-center">
+          <Text
+            TextClassProps=""
+            Text={nonProfitSection.textTwo}
+            Style="normal"
+          />
+        </div>
+      </div>
+
+      <div className="align-center mt-10 hidden w-full flex-col gap-7 text-[#111827] lg:flex">
+        <div className="flex justify-center">
+          <ExportedImage
+            src={CandidSeal}
+            alt="Candid Gold Transparency Seal"
+            className="h-[120px] w-auto object-contain"
+            basePath={basePath}
+          />
+        </div>
+
+        <div className="align-center m-auto max-w-4xl text-center font-bold">
+          <Text
+            Text={nonProfitSection.title}
+            Style="small"
+            TextClassProps=""
+          />
+        </div>
+
+        <div className="align-center m-auto max-w-4xl text-center">
+          <Text
+            TextClassProps=""
+            Text={nonProfitSection.textOne}
+            Style="normalCardHome"
+          />
+        </div>
+
+        <div className="align-center m-auto max-w-4xl text-center">
+          <Text
+            TextClassProps=""
+            Text={nonProfitSection.textTwo}
+            Style="normalCardHome"
+          />
+        </div>
+      </div>
+    </>
+  );
 }
