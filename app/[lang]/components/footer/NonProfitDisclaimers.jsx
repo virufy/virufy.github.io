abdb9020a45485ef88276e1c39383b330b001e5f
Evan Luong <evantluong105@gmail.com>
2026-06-03 22:34:56 -0700

    Add Candid transparency seal to footer

--- app/[lang]/components/footer/NonProfitDisclaimers.jsx
+++ app/[lang]/components/footer/NonProfitDisclaimers.jsx
@@ -1,52 +1,78 @@
 import Text from '../Text';
+import { basePath } from '@/next.config.mjs';
+import ExportedImage from 'next-image-export-optimizer';
+import { CandidSeal } from '@/public/images/footer/index';
 
-export default function NonProfitDisclaimers({nonProfitSection}) {
-    return (
-        <>
-        <div className="align-center mt-10 flex w-full flex-col gap-7 text-[#111827] lg:hidden">
-            <div className="align-center m-auto max-w-4xl text-center text-[25px]">
-              <Text Text={nonProfitSection.title} Style="" TextClassProps="" />
-            </div>
-            <div className="align-center m-auto mx-12 mt-4 max-w-4xl text-center">
-              <Text
-                TextClassProps=""
-                Text={nonProfitSection.textOne}
-                Style="normal"
-              />
-            </div>
-            <div className="mt-4 w-full border-t border-gray-300"></div>
-            <div className="align-center m-auto mx-12 mt-4 max-w-4xl text-center">
-              <Text
-                TextClassProps=""
-                Text={nonProfitSection.textTwo}
-                Style="normal"
-              />
-            </div>
-          </div>
-          <div className="align-center mt-10 hidden w-full flex-col gap-7 text-[#111827] lg:flex">
-            <div className="align-center m-auto max-w-4xl text-center font-bold">
-              <Text
-                Text={nonProfitSection.title}
-                Style="small"
-                TextClassProps=""
-              />
-            </div>
-            <div className="align-center m-auto max-w-4xl text-center">
-              <Text
-                TextClassProps=""
-                Text={nonProfitSection.textOne}
-                Style="normalCardHome"
-              />
-            </div>
-            <div className="align-center m-auto max-w-4xl text-center">
-              <Text
-                TextClassProps=""
-                Text={nonProfitSection.textTwo}
-                Style="normalCardHome"
-              />
-            </div>
-          </div>
-        </>
+export default function NonProfitDisclaimers({ nonProfitSection }) {
+  return (
+    <>
+      <div className="align-center mt-10 flex w-full flex-col gap-7 text-[#111827] lg:hidden">
+        <div className="flex justify-center">
+          <ExportedImage
+            src={CandidSeal}
+            alt="Candid Gold Transparency Seal"
+            className="h-[100px] w-auto object-contain"
+            basePath={basePath}
+          />
+        </div>
 
-    )
+        <div className="align-center m-auto max-w-4xl text-center text-[25px]">
+          <Text Text={nonProfitSection.title} Style="" TextClassProps="" />
+        </div>
+
+        <div className="align-center m-auto mx-12 mt-4 max-w-4xl text-center">
+          <Text
+            TextClassProps=""
+            Text={nonProfitSection.textOne}
+            Style="normal"
+          />
+        </div>
+
+        <div className="mt-4 w-full border-t border-gray-300"></div>
+
+        <div className="align-center m-auto mx-12 mt-4 max-w-4xl text-center">
+          <Text
+            TextClassProps=""
+            Text={nonProfitSection.textTwo}
+            Style="normal"
+          />
+        </div>
+      </div>
+
+      <div className="align-center mt-10 hidden w-full flex-col gap-7 text-[#111827] lg:flex">
+        <div className="flex justify-center">
+          <ExportedImage
+            src={CandidSeal}
+            alt="Candid Gold Transparency Seal"
+            className="h-[120px] w-auto object-contain"
+            basePath={basePath}
+          />
+        </div>
+
+        <div className="align-center m-auto max-w-4xl text-center font-bold">
+          <Text
+            Text={nonProfitSection.title}
+            Style="small"
+            TextClassProps=""
+          />
+        </div>
+
+        <div className="align-center m-auto max-w-4xl text-center">
+          <Text
+            TextClassProps=""
+            Text={nonProfitSection.textOne}
+            Style="normalCardHome"
+          />
+        </div>
+
+        <div className="align-center m-auto max-w-4xl text-center">
+          <Text
+            TextClassProps=""
+            Text={nonProfitSection.textTwo}
+            Style="normalCardHome"
+          />
+        </div>
+      </div>
+    </>
+  );
 }
