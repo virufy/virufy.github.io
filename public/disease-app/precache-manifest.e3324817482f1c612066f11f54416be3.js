52d41eb1bd95216508ed9e20e28a90861dd4207d
ch-ayesha <ayesha.76e@gmail.com>
2026-09-14 11:35:14 +0400

    serving disease app + disease map

---
+++ public/disease-app/precache-manifest.e3324817482f1c612066f11f54416be3.js
@@ -0,0 +1,390 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8377d50fc8e72ee32670e68b29389bbc",
+    "url": "/disease-app/index.html"
+  },
+  {
+    "revision": "2d3e61071689f83fa5e0",
+    "url": "/disease-app/static/css/13.f56b1b48.chunk.css"
+  },
+  {
+    "revision": "95056c76e7d3a9f74ee7",
+    "url": "/disease-app/static/css/52.8d4cdce2.chunk.css"
+  },
+  {
+    "revision": "50b571141ae186a544f6",
+    "url": "/disease-app/static/js/0.602c40b1.chunk.js"
+  },
+  {
+    "revision": "783f14fa45b10e088e68f98251448010",
+    "url": "/disease-app/static/js/0.602c40b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec647cf81987431a2967",
+    "url": "/disease-app/static/js/1.5b2e80f2.chunk.js"
+  },
+  {
+    "revision": "e870336ecba3abdb1c89",
+    "url": "/disease-app/static/js/10.4fd60188.chunk.js"
+  },
+  {
+    "revision": "0a01b9a27e9ec1e7ee73",
+    "url": "/disease-app/static/js/11.49b68959.chunk.js"
+  },
+  {
+    "revision": "9d82576acef76835000a",
+    "url": "/disease-app/static/js/12.b089a522.chunk.js"
+  },
+  {
+    "revision": "2d3e61071689f83fa5e0",
+    "url": "/disease-app/static/js/13.035299ba.chunk.js"
+  },
+  {
+    "revision": "a09afd57099dc21e9263ff61a08dff24",
+    "url": "/disease-app/static/js/13.035299ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d94cf5bf75aea88b35d0",
+    "url": "/disease-app/static/js/14.4482f341.chunk.js"
+  },
+  {
+    "revision": "846b7898f3c8d345ee97",
+    "url": "/disease-app/static/js/15.87538530.chunk.js"
+  },
+  {
+    "revision": "5e587d502c66829a729d",
+    "url": "/disease-app/static/js/16.ef6c3592.chunk.js"
+  },
+  {
+    "revision": "90f7b80681ec3113a3fb",
+    "url": "/disease-app/static/js/17.a93e5fca.chunk.js"
+  },
+  {
+    "revision": "445b43a62bd1b4965067",
+    "url": "/disease-app/static/js/18.bf6f2e6d.chunk.js"
+  },
+  {
+    "revision": "d2c88f0c15146cbc3cdd",
+    "url": "/disease-app/static/js/19.3e90dcf8.chunk.js"
+  },
+  {
+    "revision": "a9f9b64626610bedd830",
+    "url": "/disease-app/static/js/2.51886e1f.chunk.js"
+  },
+  {
+    "revision": "942256e9d9247d0a2abd",
+    "url": "/disease-app/static/js/20.fc364cf6.chunk.js"
+  },
+  {
+    "revision": "99f84ead19ed0ae7fa55",
+    "url": "/disease-app/static/js/21.c6cc18b5.chunk.js"
+  },
+  {
+    "revision": "26bd9bc559a0621b878e",
+    "url": "/disease-app/static/js/22.e820a152.chunk.js"
+  },
+  {
+    "revision": "dc82e36fa575dd1a3330",
+    "url": "/disease-app/static/js/23.bab5efc8.chunk.js"
+  },
+  {
+    "revision": "e4d2455aa240f99ceaf7",
+    "url": "/disease-app/static/js/24.335f2a5b.chunk.js"
+  },
+  {
+    "revision": "69b13bb0c10c2b292a65",
+    "url": "/disease-app/static/js/25.dd398451.chunk.js"
+  },
+  {
+    "revision": "52787eaf121a3bc89e48",
+    "url": "/disease-app/static/js/26.3146fabc.chunk.js"
+  },
+  {
+    "revision": "f4eec322c45a31bc9df1",
+    "url": "/disease-app/static/js/27.bb82b5b4.chunk.js"
+  },
+  {
+    "revision": "542e738646ae9eeb8ac0",
+    "url": "/disease-app/static/js/28.be334d65.chunk.js"
+  },
+  {
+    "revision": "c36d537616aa750a03fd",
+    "url": "/disease-app/static/js/29.bf78251b.chunk.js"
+  },
+  {
+    "revision": "85b6c3f4ebd0caf68533",
+    "url": "/disease-app/static/js/3.05622f16.chunk.js"
+  },
+  {
+    "revision": "8631ede9735351e10e54",
+    "url": "/disease-app/static/js/30.4c46e0ad.chunk.js"
+  },
+  {
+    "revision": "6e4801d64cd5677e977179501d49a9ce",
+    "url": "/disease-app/static/js/30.4c46e0ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04418ca24bfd16416490",
+    "url": "/disease-app/static/js/31.fe6fd1ab.chunk.js"
+  },
+  {
+    "revision": "190474427b65fd7004a4",
+    "url": "/disease-app/static/js/32.9f4840a4.chunk.js"
+  },
+  {
+    "revision": "85a556856fd63a19c4b1",
+    "url": "/disease-app/static/js/33.15f014b7.chunk.js"
+  },
+  {
+    "revision": "16a42cd002f315e82b6a",
+    "url": "/disease-app/static/js/34.050d7db6.chunk.js"
+  },
+  {
+    "revision": "cd54b298e0672f228aa0",
+    "url": "/disease-app/static/js/35.c0cb1917.chunk.js"
+  },
+  {
+    "revision": "5b97921a66ae3e1d8dd6",
+    "url": "/disease-app/static/js/36.6390ef0a.chunk.js"
+  },
+  {
+    "revision": "e698accc06c740210355",
+    "url": "/disease-app/static/js/37.c8f9cfdb.chunk.js"
+  },
+  {
+    "revision": "76a41eb49a09e5acd067",
+    "url": "/disease-app/static/js/38.191a0a1d.chunk.js"
+  },
+  {
+    "revision": "8460983c1f4157f00280",
+    "url": "/disease-app/static/js/39.19d34553.chunk.js"
+  },
+  {
+    "revision": "c53a877410d523ce002b",
+    "url": "/disease-app/static/js/4.7269f2ad.chunk.js"
+  },
+  {
+    "revision": "c360eff85ca112d098c080b19dff9b78",
+    "url": "/disease-app/static/js/4.7269f2ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99b264a17ca142cb21de",
+    "url": "/disease-app/static/js/40.fee8f2ea.chunk.js"
+  },
+  {
+    "revision": "f26f7ff236fb38b4a960",
+    "url": "/disease-app/static/js/41.e005a138.chunk.js"
+  },
+  {
+    "revision": "fb463bf99692dfc904a6",
+    "url": "/disease-app/static/js/42.6123f9b8.chunk.js"
+  },
+  {
+    "revision": "a38b35473e0a200125c9",
+    "url": "/disease-app/static/js/43.a8f3f0e7.chunk.js"
+  },
+  {
+    "revision": "e8ef09124e5b0f987fb7",
+    "url": "/disease-app/static/js/44.aef77d78.chunk.js"
+  },
+  {
+    "revision": "9272d84f1535a5866184",
+    "url": "/disease-app/static/js/45.72c7598e.chunk.js"
+  },
+  {
+    "revision": "2d68f2c308f345531fcc",
+    "url": "/disease-app/static/js/46.044c1849.chunk.js"
+  },
+  {
+    "revision": "ad7e12e4d534b8c0e2e4",
+    "url": "/disease-app/static/js/47.ce71dca2.chunk.js"
+  },
+  {
+    "revision": "e3c076ec6fc9ba8de378",
+    "url": "/disease-app/static/js/48.cf457464.chunk.js"
+  },
+  {
+    "revision": "a7ee50e9e9646136141a",
+    "url": "/disease-app/static/js/49.89facf8e.chunk.js"
+  },
+  {
+    "revision": "53ca4bd0f65b542cd679",
+    "url": "/disease-app/static/js/5.bf7bb674.chunk.js"
+  },
+  {
+    "revision": "95056c76e7d3a9f74ee7",
+    "url": "/disease-app/static/js/52.ba150b49.chunk.js"
+  },
+  {
+    "revision": "f6a33b08b8dab27dd8ab525dc9994832",
+    "url": "/disease-app/static/js/52.ba150b49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0034e801b3b8cb7feaf8",
+    "url": "/disease-app/static/js/53.f634ca15.chunk.js"
+  },
+  {
+    "revision": "9a73b17d3f74e8a595e1554cd96c873a",
+    "url": "/disease-app/static/js/53.f634ca15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbe5d14f796a783f882e",
+    "url": "/disease-app/static/js/54.ac1bc167.chunk.js"
+  },
+  {
+    "revision": "405bad065f0c1f283259",
+    "url": "/disease-app/static/js/6.255354b2.chunk.js"
+  },
+  {
+    "revision": "77a284807168e3e496f2",
+    "url": "/disease-app/static/js/7.e825ae8d.chunk.js"
+  },
+  {
+    "revision": "6a2856bc5bd61ead796d",
+    "url": "/disease-app/static/js/8.6930740d.chunk.js"
+  },
+  {
+    "revision": "f0b8eefe40d5cd0ed231",
+    "url": "/disease-app/static/js/9.ebcfa158.chunk.js"
+  },
+  {
+    "revision": "242ae545ad019ef446dd",
+    "url": "/disease-app/static/js/main.e892c3f2.chunk.js"
+  },
+  {
+    "revision": "f83aa866621d998d98ab",
+    "url": "/disease-app/static/js/runtime-main.4624d97f.js"
+  },
+  {
+    "revision": "74682ec33acb1a883ae7d81e961610ce",
+    "url": "/disease-app/static/media/BikoBold.74682ec3.woff"
+  },
+  {
+    "revision": "8dc1f108ff47b9b3a43b00d967d98c12",
+    "url": "/disease-app/static/media/BikoBold.8dc1f108.woff2"
+  },
+  {
+    "revision": "6975fe7d57dfcc2f3e8363645e37159e",
+    "url": "/disease-app/static/media/SFProDisplay.6975fe7d.woff2"
+  },
+  {
+    "revision": "8fa494e339ff1ff5b7f9c9671b1a26ca",
+    "url": "/disease-app/static/media/SFProDisplay.8fa494e3.woff"
+  },
+  {
+    "revision": "fd9a5cc9b3f09d362c672a84c05fcf44",
+    "url": "/disease-app/static/media/about-us.fd9a5cc9.svg"
+  },
+  {
+    "revision": "c2779bc8d9ccdf14e2a52cad03612d4a",
+    "url": "/disease-app/static/media/arrowLeft.c2779bc8.svg"
+  },
+  {
+    "revision": "930dedcf70ec8c30a54040cce0735f89",
+    "url": "/disease-app/static/media/arrowRight.930dedcf.svg"
+  },
+  {
+    "revision": "3463b1231a0e0d61850d21ea574ccbc8",
+    "url": "/disease-app/static/media/arrowUp.3463b123.svg"
+  },
+  {
+    "revision": "1c699025de69fb44e6b6f122da8a9403",
+    "url": "/disease-app/static/media/breath-right.1c699025.svg"
+  },
+  {
+    "revision": "6ecec3b384fe7a13e1175c1f14f7f27a",
+    "url": "/disease-app/static/media/checkCircle.6ecec3b3.svg"
+  },
+  {
+    "revision": "b99f22677b06315f084b7d3992c5e8ca",
+    "url": "/disease-app/static/media/chevron.b99f2267.svg"
+  },
+  {
+    "revision": "1d9670476a07b34f7b15dc461738fe57",
+    "url": "/disease-app/static/media/chevronRight.1d967047.svg"
+  },
+  {
+    "revision": "bf796389c4b7b44c9339d9ebf6d0696a",
+    "url": "/disease-app/static/media/clouds.bf796389.svg"
+  },
+  {
+    "revision": "5a8b85d60f5cce4d14d00f4682f2dd4e",
+    "url": "/disease-app/static/media/cough-right.5a8b85d6.svg"
+  },
+  {
+    "revision": "1f82352143848bd5603410d8cd217d58",
+    "url": "/disease-app/static/media/cross.1f823521.svg"
+  },
+  {
+    "revision": "95bd6a00c986639e560869aa014b6a74",
+    "url": "/disease-app/static/media/crossPurple.95bd6a00.svg"
+  },
+  {
+    "revision": "9c52a49bfbecd3f5f683c8dfc2bb574f",
+    "url": "/disease-app/static/media/curved-arrow.9c52a49b.svg"
+  },
+  {
+    "revision": "d0a5ec7ade66fe403cdf3cadb43b4614",
+    "url": "/disease-app/static/media/download.d0a5ec7a.svg"
+  },
+  {
+    "revision": "7bdc8e6b16c7ae0f774b47c874e3266e",
+    "url": "/disease-app/static/media/exclamationCircle.7bdc8e6b.svg"
+  },
+  {
+    "revision": "dbdb5924243b1b65edd15262f35da914",
+    "url": "/disease-app/static/media/instagram.dbdb5924.svg"
+  },
+  {
+    "revision": "77eda7d60e874564882aee7f1a0f3375",
+    "url": "/disease-app/static/media/linkedIn.77eda7d6.svg"
+  },
+  {
+    "revision": "2062462147fb3c2a8d2b11cab1ceca16",
+    "url": "/disease-app/static/media/pause.20624621.svg"
+  },
+  {
+    "revision": "05b8cc9d254814dd5a8bfe6f28a1f1f3",
+    "url": "/disease-app/static/media/play.05b8cc9d.svg"
+  },
+  {
+    "revision": "71c8da0843b5300570d8c413e7b5c134",
+    "url": "/disease-app/static/media/processing.71c8da08.svg"
+  },
+  {
+    "revision": "79439f265fca6221194a5267513fdf68",
+    "url": "/disease-app/static/media/social-distancing.79439f26.svg"
+  },
+  {
+    "revision": "56f6c7e380f07968607151ef298b8bb8",
+    "url": "/disease-app/static/media/start.56f6c7e3.svg"
+  },
+  {
+    "revision": "36f389ae6ca2ed92fa90017184a21dec",
+    "url": "/disease-app/static/media/stop.36f389ae.svg"
+  },
+  {
+    "revision": "e229a067824658fa3d91a3cfd4ae5467",
+    "url": "/disease-app/static/media/twitter.e229a067.svg"
+  },
+  {
+    "revision": "8ab18ba2fd039341c46618dfc46a83f2",
+    "url": "/disease-app/static/media/upload.8ab18ba2.svg"
+  },
+  {
+    "revision": "f709f19d816590c2b321c831b78c1bf5",
+    "url": "/disease-app/static/media/virufyLogo3x.f709f19d.png"
+  },
+  {
+    "revision": "de7a9b3f96300b62f6d6bc7349692714",
+    "url": "/disease-app/static/media/virufyLogoWhiteBox.de7a9b3f.svg"
+  },
+  {
+    "revision": "457683cbc565789dd2f60d7cbaabaac7",
+    "url": "/disease-app/static/media/voice-right.457683cb.svg"
+  },
+  {
+    "revision": "aa5b9a11703b1d39a231ec10a02352e4",
+    "url": "/disease-app/static/media/womanWithPhoneSide.aa5b9a11.svg"
+  }
+]);
